9b2c87980e9b01b17414b3fa603e0edd704821f8
Irina-Bebenina <Irs.snoopy@gmail.com>
2020-08-02 13:57:41 +0300

    добавление файлов

---
+++ src/Miles.java
@@ -0,0 +1,14 @@
+public class Miles {
+    public static void main(String[] args) {
+
+        int ticket = 10000;
+
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+
+        System.out.println(miles);
+
+    }
+
+}
